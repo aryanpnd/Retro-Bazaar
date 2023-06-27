a27128a3307deabebb0b1e57d1f376089c08f247
SHASHWAT-SGH <vnsshashwat@gmail.com>
2023-06-27 18:21:08 +0530

    added otp validation

--- frontend/dummy/src/components/OtpVerification.jsx
+++ frontend/dummy/src/components/OtpVerification.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react'
 import "../styles/otpVerification.css"
 import OTPInput from "otp-input-react";
 
+// Phoone Auth
+import { auth } from "../config/firebase.config";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+
 export default function OtpVerification() {
 
     const [phoneNumber, setPhoneNumber] = useState("");
@@ -9,30 +13,98 @@ export default function OtpVerification() {
     const [footerBtnType, setFooterBtnType] = useState('Send OTP')
     const [otpMessage, setOtpMessage] = useState({msg:"", success:true})
 
-    const handlePhoneChange = (digit) => {
-        setPhoneNumber(digit)
-    }
     const handleOtpChange = (digit) => {
         setOtp(digit)
     }
 
     const handleSendOtpBtn = () => {
-        // console.log(phoneNumber)
-        if(phoneNumber.length !== 10){
-            setOtpMessage({msg:"Invalid phone number", success:false})
-        } else{
-            setOtpMessage({msg:"OTP sent successfully", success:true})
-            setFooterBtnType("Submit")
+        if(!loading){
+            console.log(phoneNumber)
+            if(phoneNumber.toString().length !== 10){
+                setOtpMessage({msg:"Invalid phone number", success:false})
+            } else{
+                // setOtpMessage({msg:"OTP sent successfully", success:true})
+                setFooterBtnType("Submit")
+                onSignup();
+            }
         }
+        
     }
 
     const handleOtpSubmit = () => {
-        if(otp.length!==4){
-            setOtpMessage({msg:"Invalid OTP", success:false})
-        } else if(otp==="8080"){
-            setOtpMessage({msg:"Phone number verified", success:true})
+        if(!loading){
+            if(otp.length!==6){
+                setOtpMessage({msg:"Invalid OTP", success:false})
+            } else {
+                onOTPVerify();
+            }
         }
+        
     }
+
+
+
+    // Phone Auth
+
+    const [loading,setLoading] = useState(false);
+
+    function onCaptchVerify() {
+        if (!window.recaptchaVerifier) {
+          window.recaptchaVerifier = new RecaptchaVerifier(
+            "recaptcha-container",
+            {
+              size: "invisible",
+              callback: (response) => {
+                onSignup();
+              },
+              "expired-callback": () => {},
+            },
+            auth
+          );
+        }
+      }
+
+      function onSignup() {
+        setLoading(true);
+        onCaptchVerify();
+    
+        const appVerifier = window.recaptchaVerifier;
+    
+        const formatPh = "+91" + phoneNumber;
+
+        
+        setOtpMessage({msg:"Sending OTP...", success:true})
+
+        signInWithPhoneNumber(auth, formatPh, appVerifier)
+          .then((confirmationResult) => {
+            window.confirmationResult = confirmationResult;
+            setLoading(false);
+            // setShowOTP(true);
+            // toast.success("OTP sended successfully!");
+            setOtpMessage({msg:"OTP sent successfully", success:true})
+          })
+          .catch((error) => {
+            console.log(error);
+            setLoading(false);
+          });
+      }
+
+      function onOTPVerify() {
+        setLoading(true);
+        window.confirmationResult
+          .confirm(otp)
+          .then(async (res) => {
+            // console.log(res);
+            setOtpMessage({msg:"Phone number verified.", success:true})
+            // setUser(res.user);
+            setLoading(false);
+          })
+          .catch((err) => {
+            console.log(err);
+            setOtpMessage({msg:"Invalid OTP.", success:false})
+            setLoading(false);
+          });
+      }
 
   return (
     <div className="otp-modal-wrapper">
@@ -42,9 +114,16 @@ export default function OtpVerification() {
             </div>
             <div className="number-input-field-conatiner">
             
-                {footerBtnType==="Send OTP"?<OTPInput className='number-input-field' value={phoneNumber} onChange={handlePhoneChange} autoFocus OTPLength={10} otpType="number" disabled={false} />
-                :<OTPInput className='number-input-field' value={otp} onChange={handleOtpChange} autoFocus OTPLength={4} otpType="number" disabled={false} />}
+                {footerBtnType==="Send OTP"
+                ? <input autoFocus type="number" className='tel-Input-for-otp' onChange={(e)=> setPhoneNumber(e.target.value)}/>
+                // <OTPInput className='number-input-field' value={phoneNumber} onChange={handlePhoneChange} autoFocus OTPLength={10} otpType="number" disabled={false} />
+                :<OTPInput className='number-input-field' value={otp} onChange={handleOtpChange} autoFocus OTPLength={6} otpType="number" disabled={false} />}
             </div>
+
+            <div className="captcha-container" id='recaptcha-container'>
+                
+            </div>
+
             <div className='otp-footer'>
                 <span className='otp-message'
                     style={{color:otpMessage.success?'green':'red'}}
@@ -59,8 +138,10 @@ export default function OtpVerification() {
                     Back
                 </span>
 
-                <span className='send-otp-btn' 
-                  onClick={footerBtnType==="Send OTP"?handleSendOtpBtn:handleOtpSubmit}
+                <span className='send-otp-btn'
+                  onClick={
+                    footerBtnType==="Send OTP"?handleSendOtpBtn:handleOtpSubmit}
+                    style={{cursor:loading?'not-allowed':'pointer'}}
                 >
                     {footerBtnType}
                 </span>
