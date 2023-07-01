e0cee1cd831b5a3c38bbeca3bb0f342381fd3a5e
aryanpnd <captainaryan9870@gmail.com>
2023-07-01 09:25:35 +0530

    CORS error solve and api integration

--- frontend/main/src/components/home/Item.jsx
+++ frontend/main/src/components/home/Item.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 // import { Heart, HeartOutline } from 'react-ionicons'
 import '../../css/Item.css'
+import { HeartFilled, HeartOutlined } from '@ant-design/icons'
 
-function Item({ id, name, description, price, date, image }) {
+function Item({ name, description, price, category, date, image, userImage,userName }) {
 
     const [wishlist, setWishlist] = useState(false)
 
@@ -19,24 +20,24 @@ function Item({ id, name, description, price, date, image }) {
                         alt=''
                     />
                     <div className='item-img-wishlist' onClick={toggleWishlist}>
-                        {/* {wishlist ? 
-                        <Heart
-                            color={'#ff2171'}
-                            title={"heart"}
-                            height="2rem"
-                            width="2rem"
-                        /> : 
-                        <HeartOutline
-                            color={'#ff2171'}
-                            title={"heart"}
-                            height="2rem"
-                            width="2rem"
-                        />
-                        } */}
+                        {wishlist ?
+                            <HeartFilled style={{ color: 'red' }}
+                                color={'red'}
+                                title={"heart"}
+                                height="2rem"
+                                width="2rem"
+                            /> :
+                            <HeartOutlined
+                                color={'#ff2171'}
+                                title={"heart"}
+                                height="2rem"
+                                width="2rem"
+                            />
+                        }
                     </div>
 
                 </div>
-                <div style={{ display: 'flex', justifyContent: "space-between", alignItems: "center", margin: "5%" }}>
+                <div className='item-title-header-container'>
                     <div className="item-title">
                         {name}
                     </div>
@@ -44,8 +45,16 @@ function Item({ id, name, description, price, date, image }) {
                         {price}
                     </div>
                 </div>
-                <div className="item-wishlist-button">
-                    <button>Chat Now</button>
+                <div className="item-description-container">
+                    <div className='item-description'>{description}</div>
+                </div>
+                <div className="item-date-and-category-container">
+                    <div className='item-category'>{category}</div>
+                    <div className='item-date'>{date.split('T')[0]}</div>
+                </div>
+                <div className="item-Bottom-container">
+                    <button className='item-view-now-btn'>Chat Now</button>
+                    <img className='item-user-profile' style={{ height: "2.2rem", width: "2.2rem", borderRadius: "100%" }} src={`${userImage ? userImage : `https://ui-avatars.com/api/?name=${userName}&background=e91e63&color=fff&rounded=true`}`} alt='' />
                 </div>
             </div>
         </>
