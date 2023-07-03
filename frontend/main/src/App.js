e2a61460364f7cc1430bce117a23fb890bc02b67
aryanpnd <captainaryan9870@gmail.com>
2023-07-03 08:08:58 +0530

    API integration - products and wishlist

--- frontend/main/src/App.js
+++ frontend/main/src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Wishlist from "./components/wishlist/Wishlist";
 
-export const apiURL = "http://localhost:8080";
+export const apiURL = "";
 
 function App() {
   return (
