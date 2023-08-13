d381f6a7dd44d470e3dfcbeb19df091c498c9ef9
aryanpnd <captainaryan9870@gmail.com>
2023-08-13 11:16:54 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,13 @@ const app = express();
 
 // initializing app middlewares
 app.use(
-  // cors({
+  cors(
+  //   {
   //   origin: "http://localhost:3000", // allow to server to accept request from different origin
   //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   //   credentials: true, // allow session cookie from browser to pass through
-  // })
+  // }
+  )
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
